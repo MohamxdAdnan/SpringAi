9d5c786eb8f151c4137886a74e468d6da4a00130
Mohamed Adnan <mohamxd.adnan@gmail.com>
2026-04-01 16:09:13 +0530

    Chat options feature

--- Section01/springAi/src/main/java/com/helloWorld/springAi/config/ChatClientConfig.java
+++ Section01/springAi/src/main/java/com/helloWorld/springAi/config/ChatClientConfig.java
@@ -3,6 +3,7 @@ package com.helloWorld.springAi.config;
 import com.helloWorld.springAi.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +14,12 @@ public class ChatClientConfig {
 
     @Bean
     ChatClient chatClient (ChatClient.Builder chatClientBuilder){
+
+        ChatOptions chatOptions=ChatOptions.builder().model("gpt-4.1-mini")
+                .temperature(0.8).build();
+
         return chatClientBuilder
+                .defaultOptions(chatOptions)
                 .defaultAdvisors(List.of(new SimpleLoggerAdvisor(),
                         new TokenUsageAuditAdvisor()))
                 .defaultSystem("""
